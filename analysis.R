6282403dbc6422836c6a59fd96e16bd905142b56
Tadas Danielius <tadasda@lt.ibm.com>
2016-11-21 17:04:51 +0200

    analysis in progress

--- analysis.R
+++ analysis.R
@@ -11,13 +11,13 @@ plot(eeg.event.data.smooth, main='Smoothed Data')
 # Variance 
 par(mfrow = c(1,1))
 eeg.var = func.var(eeg.event.data.smooth)
+
 plot(eeg.var, main='Variance')
 
 # Mean
 par(mfrow = c(1,1))
 eeg.event.data.mean = func.mean(eeg.event.data.smooth)
 plot(eeg.event.data.mean, main='Mean')
-
 
 
 # Derivatives
@@ -63,13 +63,14 @@ eeg.plot$plot.clusters.charts(eeg.deriv, eeg.deriv.clusters2$cluster)
 
 # FPCA
 eeg.event.data.smooth.fd = fdata(eeg.event.data.smooth)
-eeg.event.data.pca3 = fdata2pc(eeg.event.data.smooth.fd,ncomp=3,l=5)
+eeg.event.data.pca3 = fdata2pc(eeg.event.data.smooth.fd,ncomp=3)
 summary(eeg.event.data.pca3)
 
 # FPCA deriv
-eeg.deriv.pca3 = fdata2pc(eeg.deriv,ncomp=3,l=100)
+eeg.deriv.pca3 = fdata2pc(eeg.deriv,ncomp=2)
 summary(eeg.deriv.pca3)
 
+# check white noise PCA_X1(t) - SMOOTH_X2(t)
 
 # Correlations
 time_vector=1:250
